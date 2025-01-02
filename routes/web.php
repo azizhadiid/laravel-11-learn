b267573befadd2a408dc13d55aa491cace9325bb
azizhadiid <azizalhadiid88@mgail.com>
2025-01-02 13:28:37 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/blog', function () {
+    return 'Hello World';
+});
